ce855c45220b136ce258d0bddbc8e8eca36d9a0c
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-09-20 19:58:19 -0400

    rearranged some functions to pass props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,83 @@
 import React, { Component } from "react";
 import "./App.css";
-// import axios from "axios";
+import axios from "axios";
 import Forecast from "./components/Forecast";
 
-/*NOTE: 
-ACCESS CURRENT WEATHER -> http://api.aerisapi.com/forecasts/11101?client_id=CLIENT_ID&client_secret=CLIENT_SECRET
-CLIENT: SVVhlCO2nl5rpYkVUNcXc  (ACCESS ID)
-CLIENT_SECRET: s208gxgDi3a9BYtA5z0Uu7LJc8BzedCNcdI4uQnC  (SECRET KEY)
-*/
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      weekForecast: [],
       maxTempF: null,
       minTempF: null,
+      tempChange: false,
+      tempUnit: "°F",
+      conversionMessage: "Show Celcius"
     };
   }
 
-  renderForecast(temp) {
-    return <Forecast temp={this.state.temp}/>
+  displayWeeklyForecast = e => {
+    axios
+      .get(
+        "http://api.aerisapi.com/forecasts/11101?client_id=SVVhlCO2nl5rpYkVUNcXc&client_secret=s208gxgDi3a9BYtA5z0Uu7LJc8BzedCNcdI4uQnC"
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({
+          weekForecast: res.data.response[0].periods
+        });
+      })
+      .catch(err => {
+        console.log("error message: ", err);
+      });
+  };
+
+  componentDidMount() {
+    this.displayWeeklyForecast();
   }
+
+  changeTempUnit = () => {
+    const { tempChange } = this.state;
+    if (tempChange === false) {
+      this.setState({
+        tempChange: true,
+        tempUnit: "°C",
+        conversionMessage: "Show Fahrenheit"
+      });
+    } else {
+      this.setState({
+        tempChange: false,
+        tempUnit: "°F",
+        conversionMessage: "Show Celcius"
+      });
+    }
+  };
+
   render() {
+    const {
+      conversionMessage,
+      maxTempF,
+      minTempF,
+      tempUnit,
+      weekForecast
+    } = this.state;
+
+    console.log(this.state.maxTempF, "max");
+    console.log(this.state, "state");
     return (
       <div className="App">
-        <input type="button" value="Show Fahrenheit" />
-        <Forecast temp={this.state.temp} />
+        <input
+          type="button"
+          value={conversionMessage}
+          onClick={this.changeTempUnit}
+        />
+        <Forecast
+          tempUnit={tempUnit}
+          weekForecast={weekForecast}
+          maxTempF={maxTempF}
+          minTempF={minTempF}
+        />
       </div>
     );
   }
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,50 +1,10 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Forecast extends Component {
-  constructor(props) {
-    super();
-
-    this.state = {
-      maxTempF: null,
-      minTempF: null,
-      weekForecast: []
-    };
-  }
-
-  displayWeeklyForecast = e => {
-    axios
-      .get(
-        "http://api.aerisapi.com/forecasts/11101?client_id=SVVhlCO2nl5rpYkVUNcXc&client_secret=s208gxgDi3a9BYtA5z0Uu7LJc8BzedCNcdI4uQnC"
-      )
-      .then(res => {
-        console.log(res);
-        this.setState({
-          weekForecast: res.data.response[0].periods
-        });
-      })
-      .catch(err => {
-        console.log("error message: ", err);
-      });
-  };
-
-  componentDidMount() {
-    this.displayWeeklyForecast();
-  }
-
-  retrieveTemp = e => {
-    this.setState({
-      maxTempF: e.target.value,
-      minTempF: e.target.value
-    });
-  };
-
   render() {
-    const { weekForecast } = this.state;
-
     return (
       <section className="weekly-forecast">
-        {weekForecast.map(dayForecast => {
+        {this.props.weekForecast.map(dayForecast => {
           return (
             <article className="day-forecast" key={dayForecast.timestamp}>
               <div className="date">{dayForecast.dateTimeISO.slice(0, 10)}</div>
@@ -57,12 +17,12 @@ class Forecast extends Component {
               />
 
               <div className="temperature">
-              <div value={dayForecast.maxTempF} name="maxTempF">
-                High: {dayForecast.maxTempF}{" "}
-              </div>
-              <div value={dayForecast.minTempF} name="minTempC">
-                Low: {dayForecast.minTempF}{" "}
-              </div>
+                <div>
+                  High: {`${dayForecast.maxTempF} ${this.props.tempUnit}`}
+                </div>
+                <div>
+                  Low: {`${dayForecast.minTempF} ${this.props.tempUnit}`}
+                </div>
               </div>
             </article>
           );
